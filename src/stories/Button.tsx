936feb49379f5f2204fc7a00c8f9bf1959e9092d
Joao Almeida <joao-almeida11@outlook.com>
2025-06-25 01:55:04 +0100

    feat(ui): button

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,12 +1,8 @@
-import React from "react";
-
-import "./button.css";
-
 export interface ButtonProps {
   /** Is this the principal call to action on the page? */
   primary?: boolean;
   /** What background color to use */
-  backgroundColor?: string;
+  // backgroundColor?: string;
   /** How large should the button be? */
   size?: "small" | "medium" | "large";
   /** Button contents */
@@ -17,22 +13,36 @@ export interface ButtonProps {
 
 /** Primary UI component for user interaction */
 export const Button = ({
-  primary = false,
+  primary = true,
   size = "medium",
-  backgroundColor,
+  // backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+  const mode = primary ? "bg-uplife-primary text-zinc-900" : "";
+
+  let currentSize;
+  switch (size) {
+    case "small":
+      currentSize = "py-2 px-4";
+      break;
+    case "medium":
+      currentSize = "py-3 px-5";
+      break;
+    case "large":
+      currentSize = "py-4 px-6";
+      break;
+  }
+
   return (
     <button
       type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " ",
-      )}
-      style={{ backgroundColor }}
+      className={[
+        "inline-block cursor-pointer border-0 font-bold text-base bg-sky-500 font-sans text-zinc-900 rounded-lg",
+        `storybook-button--${size}`,
+        mode,
+        currentSize,
+      ].join(" ")}
       {...props}
     >
       {label}
